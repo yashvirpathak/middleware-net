efed3b2ca5f08a53cfff9cd251407d93c080724b
yashvirpathak <yashvir.pathak@gmail.com>
2022-03-08 19:13:34 +0530

    updated package.json

--- application/weatherdevice-app/deviceModel.js
+++ application/weatherdevice-app/deviceModel.js
@@ -1,3 +1,8 @@
+/**
+ * Function to map message fields into Data model
+ * @param {message} message - message to be parsed 
+ * @returns 
+ */
 let deviceModel = (message) => {
     let model = {
         deviceId: message.device_id,
